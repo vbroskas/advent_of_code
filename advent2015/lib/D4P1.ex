bcc2a0694964f7945d33191c042283ed72a854f6
loren <baca.loren@gmail.com>
2020-12-03 14:19:20 -0700

    cleaned up solution for day 4

--- advent2015/lib/D4P1.ex
+++ advent2015/lib/D4P1.ex
@@ -4,15 +4,10 @@ defmodule D4P1 do
   """
   def find_lowest() do
     input = "ckczppom"
-    number = 1
-
-    <<head::bytes-size(5), rest::binary>> =
-      :crypto.hash(:md5, "#{input}#{number}") |> Base.encode16()
-
-    calc_num(input, number, head)
+    calc_num(input)
   end
 
-  def calc_num(input, number \\ 1, head \\ "")
+  def calc_num(input, number \\ 0, head \\ "")
 
   def calc_num(_input, number, "000000") do
     number
