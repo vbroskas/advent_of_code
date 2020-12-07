86850606110326556c55323962bf632c5d331e14
loren <baca.loren@gmail.com>
2020-12-06 17:09:34 -0700

    d6p1 almost done

--- advent2015/lib/D6P1.ex
+++ advent2015/lib/D6P1.ex
@@ -1,8 +1,8 @@
 defmodule D6P1 do
   # TODO change this from 5 to 999 in final solution
-  @initial_off for x <- 0..5, y <- 0..5, do: [x, y]
+  @initial_off for x <- 0..999, y <- 0..999, do: [x, y]
   @initial_on []
-  @rejects ["turn", "through"]
+  @rejects ["turn", "through", "\n"]
 
   def get_instructions() do
     {num_lights_on, _current_on, _current_off} =
@@ -10,11 +10,12 @@ defmodule D6P1 do
       |> Path.expand(__DIR__)
       |> File.stream!()
       |> Stream.map(&String.replace(&1, @rejects, ""))
-      |> Stream.map(&String.trim/1)
-      |> Stream.map(&String.split/1)
+      |> Stream.map(&String.split(&1, [" ", ","], trim: true))
       |> Enum.reduce({0, @initial_on, @initial_off}, fn x, {_acc, current_on, current_off} ->
         calc_lights_on(x, current_on, current_off)
       end)
+
+    num_lights_on
   end
 
   def calc_lights_on(instructions, current_on \\ @initial_on, current_off \\ @initial_off)
@@ -22,21 +23,25 @@ defmodule D6P1 do
   def calc_lights_on(
         [
           "toggle",
-          <<x_start::bytes-size(3), ",", y_start::binary>>,
-          <<x_end::bytes-size(3), ",", y_end::binary>>
+          x_start,
+          y_start,
+          x_end,
+          y_end
         ],
         current_on,
         current_off
       ) do
     temp_list = create_temp_list(x_start, y_start, x_end, y_end)
-    ...
+    find_toggles(temp_list, current_on, current_off)
   end
 
   def calc_lights_on(
         [
           "on",
-          <<x_start::bytes-size(3), ",", y_start::binary>>,
-          <<x_end::bytes-size(3), ",", y_end::binary>>
+          x_start,
+          y_start,
+          x_end,
+          y_end
         ],
         current_on,
         current_off
@@ -56,8 +61,10 @@ defmodule D6P1 do
   def calc_lights_on(
         [
           "off",
-          <<x_start::bytes-size(3), ",", y_start::binary>>,
-          <<x_end::bytes-size(3), ",", y_end::binary>>
+          x_start,
+          y_start,
+          x_end,
+          y_end
         ],
         current_on,
         current_off
@@ -82,6 +89,20 @@ defmodule D6P1 do
   def add_to_list(temp_list, current_list) do
     (temp_list ++ current_list) |> Enum.uniq()
   end
+
+  def find_toggles(temp_list, current_on, current_off) do
+    # ON-----------
+    matching_on = Enum.filter(current_on, fn el -> Enum.member?(temp_list, el) end)
+    # remove matching_on from current_on, and add matching_on to current_off
+
+    # OFF---------
+    matching_off = Enum.filter(current_off, fn el -> Enum.member?(temp_list, el) end)
+    # remove matching_off from current_off, and add matching_off to current_on
+    current_off = (current_off -- matching_off) ++ matching_on
+    current_on = (current_on -- matching_on) ++ matching_off
+    num_on = Enum.count(current_on)
+    {num_on, current_on, current_off}
+  end
 end
 
 # ["toggle", "461,550", "564,900"]
@@ -96,3 +117,7 @@ end
 # turn on 376,415 through 768,548
 # turn on 606,361 through 892,600
 # turn off 448,208 through 645,684
+
+# turn on 0,0 through 4,4
+# toggle 0,0 through 1,1
+# toggle 1,1 through 2,2
